cee690d7ae027f1dadbf379c069d6abf4b53f8b0
dbsmith88 <dbsmith@uga.edu>
2018-04-20 09:53:51 -0500

    tasks data retrieval is from mongoDB instead of from file

--- tasks.py
+++ tasks.py
@@ -5,19 +5,16 @@ QED celery instance
 from __future__ import absolute_import
 import os
 import logging
-# import redis
 import json
 import uuid
 from datetime import datetime
 
-#from celery import Celery
 from flask import request, Response
 from flask_restful import Resource
 
 import pymongo as pymongo
 
 # from flask_qed.celery_cgi import celery
-#from ..celery_cgi import celery
 from celery_cgi import celery
 
 logging.getLogger('celery.task.default').setLevel(logging.DEBUG)
@@ -25,40 +22,12 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 if __name__ == "pram_flask.tasks":
     from pram_flask.ubertool.ubertool.sam import sam_exe as sam
-    from pram_flask.REST_UBER import rest_model_caller, rest_validation
-    from pram_flask.temp_config.set_environment import DeployEnv
+    from pram_flask.REST_UBER import rest_model_caller
 else:
     logging.info("SAM Task except import attempt..")
     from .ubertool.ubertool.sam import sam_exe as sam
-    from .REST_UBER import rest_model_caller, rest_validation
-    from .temp_config.set_environment import DeployEnv
+    from .REST_UBER import rest_model_caller
     logging.info("SAM Task except import complete!")
-
-# runtime_env = DeployEnv()
-# runtime_env.load_deployment_environment()
-#
-# redis_hostname = os.environ.get('REDIS_HOSTNAME')
-# redis_port = os.environ.get('REDIS_PORT')
-# REDIS_HOSTNAME = os.environ.get('REDIS_HOSTNAME')
-#
-# if not os.environ.get('REDIS_HOSTNAME'):
-#     os.environ.setdefault('REDIS_HOSTNAME', 'redis')
-#     REDIS_HOSTNAME = os.environ.get('REDIS_HOSTNAME')
-#
-# logging.info("REDIS HOSTNAME: {}".format(REDIS_HOSTNAME))
-#
-# redis_conn = redis.StrictRedis(host=REDIS_HOSTNAME, port=6379, db=0)
-
-#app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0',)
-# app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0',)
-#
-# app.conf.update(
-#     CELERY_ACCEPT_CONTENT=['json'],
-#     CELERY_TASK_SERIALIZER='json',
-#     CELERY_RESULT_SERIALIZER='json',
-#     CELERY_IGNORE_RESULT=False,
-#     CELERY_TRACK_STARTED=True,
-# )
 
 IN_DOCKER = os.environ.get("IN_DOCKER")
 
@@ -137,18 +106,14 @@ class SamRun(Resource):
 
 class SamData(Resource):
     def get(self, task_id):
-        dir_path = os.getcwd()
         logging.info("SAM data request for task id: {}".format(task_id))
-        file_path = dir_path + '/pram_flask/ubertool/ubertool/sam/bin/Results/' + str(task_id) + '/out_json.csv'
-        try:
-            logging.info("SAM data request file path: {}".format(file_path))
-            with open(file_path, 'rb') as data:
-                data_json = data.read()
-            data_json = json.dumps(json.loads(data_json))
-        except FileNotFoundError as er:
-            logging.info("SAM data file not found, data request not successful.")
-            return "{'error': 'data file not found', 'file_path': " + str(file_path) + "}"
-        logging.info("SAM data file found, data request successful.")
+        status = sam_status(task_id)
+        if status['status'] == 'SUCCESS':
+            data_json = json.dumps(status['data'])
+            logging.info("SAM data found, data request successful.")
+        else:
+            data_json = ""
+            logging.info("SAM data not available for requested task id.")
         return Response(data_json, mimetype='application/json')
 
 
